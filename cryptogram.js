caae075e4829f1529fdaed49879e2fcd62dcf5ae
Tony Larch <tony@invalid.org>
2026-09-19 09:10:10 +0100

    Initial commit

---
+++ cryptogram.js
@@ -0,0 +1,334 @@
+let startTime, solvedTime;
+
+function makeKey(keyString) {
+	let ret = new Map();
+	for (const [i, c] of Object.entries(keyString)) {
+		ret.set(c, uppercase[i]);
+	}
+	return ret;
+}
+
+function makeReverseKey(keyString) {
+	let ret = new Map();
+	for (const [i, c] of Object.entries(uppercase)) {
+		ret.set(c, keyString[i]);
+	}
+	return ret;
+}
+
+class Puzzle {
+	cipher;		// the ciphertext
+	keyString;  // the original key string
+	key;		// a map from cipher to plain
+	attempt;	// a partial map from cipher to plain
+	mapped;		// set of the values from attempt
+
+	constructor(cipher, keyString) {
+		this.cipher = cipher;
+		this.keyString = keyString;
+		this.key = makeKey(keyString);
+		this.attempt = new Map();
+		this.mapped = new Set();
+		this.me = this;
+	}
+
+	setAttempt(cipherChar, plainChar) {
+		let me = this.me;
+		me.attempt.set(cipherChar, plainChar);
+		me.mapped.add(plainChar);
+	}
+
+	removeAttempt(cipherChar) {
+		let me = this.me;
+		let plain = me.attempt.get(cipherChar);
+		me.attempt.delete(cipherChar);
+
+		if (plain !== undefined) {
+			me.mapped.delete(plain);
+		}
+	}
+
+	removeAllAttempts() {
+		let me = this.me;
+		me.attempt = new Map();
+		me.mapped = new Set();
+	}
+
+	getUnused() {
+		let ret = "";
+		for (const c of uppercase) {
+			if (!this.me.mapped.has(c)) {
+				ret += c;
+			}
+		}
+		return ret;
+	}
+
+	isSolved() {
+		let me = this.me;
+
+		for (const c of me.cipher) {
+			if (me.attempt.get(c) != me.key.get(c)) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+}
+
+let selectedCipherChar;
+
+function makeClickHandler(puzzle) {
+	return (ev) => {
+		let letter = ev.currentTarget.closest(".letter");
+		selectedCipherChar = letter.querySelector(".cipher").innerText;
+		updateDisplay(puzzle);
+	}
+}
+
+function formatDelta(t) {
+	function fmt(v) {
+		return String(v).padStart(2, '0');
+	}
+	let ret = "";
+	let seconds = Math.floor(t / 1000);
+	let hours = Math.floor(seconds / 3600);
+	if (hours != 0) {
+		ret = fmt(hours) + ":";
+	}
+	seconds -= hours * 3600;
+	let minutes = Math.floor(seconds / 60);
+	ret += fmt(minutes) + ":";
+	seconds -= minutes * 60;
+	ret += fmt(seconds);
+	return ret;
+}
+
+function keyHandler(puzzle, ev) {
+	switch (ev.key) {
+	case "Backspace":
+		puzzle.removeAttempt(selectedCipherChar);
+		break;
+	case "Delete":
+		puzzle.removeAttempt(selectedCipherChar);
+		break;
+	case "Escape":
+		selectedCipherChar = undefined;
+		puzzle.removeAllAttempts();
+		break;
+	default:
+		{
+			let val = ev.key.toUpperCase();
+			if (!uppercaseSet.has(val)) {
+				return;
+			}
+			puzzle.setAttempt(selectedCipherChar, val);
+		}
+	}
+	updateDisplay(puzzle);
+
+	if (puzzle.isSolved()) {
+		if (solvedTime === undefined) {
+			solvedTime = new Date();
+		}
+		let elapsed = solvedTime - startTime;
+		let delta = formatDelta(elapsed);
+		document.getElementById("time").innerText = delta;
+		document.getElementById("solved").showModal();
+		navigator.clipboard.writeText(`Puzzle solved in ${delta}`);
+	}
+}
+
+function displayPuzzle(puzzle) {
+	let container = document.getElementById("puzzle_container");
+	let ncTempl = document.getElementById("non_coded_letter_template");
+	let template = document.getElementById("letter_template");
+
+	function newWord() {
+		let ret = document.createElement("div");
+		ret.classList.add("word");
+		return ret;
+	}
+
+	let currentWord = newWord();
+
+	for (const c of puzzle.cipher) {
+		let templ = template.content.cloneNode(true);
+		if (uppercaseSet.has(c)) {
+			let cipher = templ.querySelector(".cipher");
+			cipher.innerText = c;
+
+			// There won't actually be any attempts right at the start.
+			let plainNode = templ.querySelector(".plain");
+			if (puzzle.attempt.has(c)) {
+				plainNode.innerText = puzzle.attempt.get(c);
+			} else {
+				plainNode.innerText = '\u00A0';
+			}
+		} else {
+			templ.querySelector(".letter").innerText = c;
+			if (c == " ") {
+				container.append(currentWord);
+				currentWord = newWord();
+			}
+		}
+		let l = templ.querySelector(".letter");
+		l.addEventListener("click", makeClickHandler(puzzle));
+		currentWord.append(templ);
+		container.append(templ);
+	}
+	container.append(currentWord);
+
+	document.addEventListener("keyup", (ev) => {
+		keyHandler(puzzle, ev);
+	});
+	document.getElementById("unused").innerText = puzzle.getUnused();
+}
+
+function updateDisplay(puzzle) {
+	let container = document.getElementById("puzzle_container");
+	let letters = container.querySelectorAll(".letter");
+	let seen = new Map();
+
+	letters.forEach((node) => {
+		let cipherNode = node.querySelector(".cipher");
+		if (cipherNode === null) {
+			return;
+		}
+		let cipher = cipherNode.innerText;
+		let plain = puzzle.attempt.get(cipher);
+
+		let plainNode = node.querySelector(".plain");
+		plainNode.classList.remove("conflicted");
+
+		if (cipher == selectedCipherChar) {
+			node.classList.add("current");
+		} else {
+			node.classList.remove("current");
+		}
+
+		if (plain !== undefined) {
+			existing = seen.get(plain);
+			if (existing !== undefined && existing != cipher) {
+				plainNode.classList.add("conflicted");
+			}
+			seen.set(plain, cipher);
+			plainNode.innerText = plain;
+		} else {
+			plainNode.innerText = '\u00A0';
+		}
+	});
+
+	document.getElementById("unused").innerText = puzzle.getUnused();
+}
+
+function createPuzzle(text) {
+	let letters = [...uppercase];
+
+	for (let i = letters.length-1; i > 0; i--) {
+		const j = Math.floor(Math.random()*(i+1));
+		[letters[i], letters[j]] = [letters[j], letters[i]];
+	}
+
+	let ret = letters.join("");
+	let key = makeReverseKey(ret);
+	text = text.toUpperCase();
+
+	for (const c of text) {
+		if (uppercaseSet.has(c)) {
+			ret += key.get(c);
+		} else {
+			ret += c;
+		}
+	}
+	return ret
+}
+
+function setupCreate() {
+	let newPuzz = document.getElementById("open_create");
+
+	newPuzz.addEventListener("click", (e) => {
+		document.getElementById("create_puzzle").showModal();
+	});
+
+	let puzzDialog = document.getElementById("create_puzzle");
+
+	puzzDialog.addEventListener("keyup", (e) => {
+		console.log("keyup");
+		e.stopPropagation();
+	});
+
+	let createText = document.getElementById("create_text");
+	createText.addEventListener("keydown", (e) => {
+		switch (e.key) {
+		case "Backspace":
+		case "Delete":
+			break;
+		case "Enter":
+			e.preventDefault();
+		default:
+			c = e.key.toUpperCase();
+			if (encodeLetter(c) === undefined) {
+				e.preventDefault();
+			}
+		}
+	});
+
+	let create = document.getElementById("create");
+	create.addEventListener("click", (e) => {
+		let text = createText.innerText;
+		let url = window.location.href.split('?')[0]
+			+ "?" + packString(createPuzzle(text))
+		navigator.clipboard.writeText(url);
+		document.getElementById("url").innerText = url;
+		document.getElementById("created").classList.remove("hidden");
+	});
+
+	document.getElementById("reset_create").addEventListener("click", (e) => {
+		document.getElementById("create_text").innerText = null;
+		document.getElementById("url").innerText = null;
+		document.getElementById("created").classList.add("hidden");
+	});
+
+	document.getElementById("close_create").addEventListener("click", (e) => {
+		puzzDialog.close();
+	});
+}
+
+function setupSuccess() {
+	let success = document.getElementById("solved");
+	let close = document.getElementById("close_success");
+	close.addEventListener("click", (e) => {
+		success.close();
+	});
+}
+
+function setupButtons(puzzle) {
+	document.getElementById("delete").addEventListener("click", (e) => {
+		puzzle.removeAttempt(selectedCipherChar);
+		updateDisplay(puzzle);
+	});
+	document.getElementById("reset").addEventListener("click", (e) => {
+		puzzle.removeAllAttempts();
+		selectedCipherChar = undefined;
+		updateDisplay(puzzle);
+	});
+}
+
+function main() {
+	initPack();
+	setupCreate();
+	setupSuccess();
+
+	let data = unpackString(window.location.search.slice(1));
+	let keyString = data.slice(0, 26);
+	let cipher = data.slice(26);
+
+	let p = new Puzzle(cipher, keyString);
+	displayPuzzle(p);
+	setupButtons(p);
+	startTime = new Date();
+}
+
+window.onload = main;
---
+++ pack.js
@@ -0,0 +1,158 @@
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let uppercaseSet = new Set();
+let punctuationCoding, reversePunctuationCoding;
+
+function initPack() {
+	for (const c of uppercase) {
+		uppercaseSet.add(c);
+	}
+
+	punctuationCoding = new Map();
+	reversePunctuationCoding = new Map();
+
+	let i = 27;
+	for (const c of " .,-\"'?!<>;:()") {
+		punctuationCoding.set(c, i);
+		reversePunctuationCoding.set(i, c);
+		i++;
+	}
+}
+
+// OK this probably gonna return an array in case you end up with two values.
+function encodeLetter(c) {
+	/*
+	 * We encode 'A' as 1, so that 0 can be used for padding (the thing you're
+	 * decoding is a multiple of 8 bits, so there may be >6 left over)
+	 */
+	if (uppercaseSet.has(c)) {
+		return c.charCodeAt(0) - 64;
+	}
+
+	let ret = punctuationCoding.get(c);
+	if (ret !== undefined) {
+		return ret;
+	}
+
+	return undefined;
+}
+
+function decodeLetter(code) {
+	let c = code + 64;
+	if (c >= 65 && c <= 90) {
+		return String.fromCodePoint(c);
+	}
+
+	return reversePunctuationCoding.get(code);
+}
+
+function b64encode(array) {
+	let s = String.fromCharCode(...array);
+
+	// Switch a few characters and get rid of any padding (the = character is
+	// problematic in URLs)
+	return btoa(s).replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
+}
+
+function b64decode(s) {
+	s = s.replace(/-/g, '+').replace(/_/g, '/');
+	while (s.length % 4) {
+		s += '=';
+	}
+	let bs = atob(s);
+	let ret = new Uint8Array(bs.length);
+	for (i = 0; i < bs.length; i++) {
+		ret[i] = bs.charCodeAt(i)
+	}
+	return ret;
+}
+
+function packString(s) {
+	const numChars = s.length;
+	const numBits = numChars * 6;
+	const numBytes = Math.ceil(numBits / 8);
+	let ret = new Uint8Array(numBytes);
+
+	let i = 0;	// current byte we're filling in
+	let j = 0;	// which bit we've got to in the output
+
+	function incr(n) {
+		j += n;
+		if (j >= 8) {
+			i++;
+			j -= 8;
+		}
+	}
+
+	function putBits(n, val) {
+		// put n bits into the current byte
+		let mask = (1<<n) - 1
+		ret[i] |= (val & mask) << j
+		incr(n);
+	}
+
+	for (let c of s) {
+		const val = encodeLetter(c);	// get the next 6-bit value
+		const avail = 8 - j;
+
+		if (avail >= 6) {
+			putBits(6, val);
+			continue;
+		}
+
+		// Put what we can into the current byte.
+		putBits(avail, val);
+
+		// And anything left-over into the next one.
+		let remaining = 6 - avail;
+		if (remaining > 0) {
+			putBits(remaining, val >> avail);
+		}
+	}
+
+	return b64encode(ret);
+}
+
+function unpackString(s) {
+	let array = b64decode(s);
+	let ret = "";
+	let i = 0, j = 0;
+
+	function incr(n) {
+		j += n;
+		if (j >= 8) {
+			i++;
+			j -= 8;
+		}
+	}
+
+	function getBits(n) {
+		// Get the next n bits from the current byte
+		const mask = (1<<n) - 1;
+		const ret = (array[i] >> j) & mask;
+		incr(n);
+		return ret;
+	}
+
+	function getCode() {
+		const avail = 8 - j;
+		if (avail >= 6) {
+			return getBits(6);
+		}
+
+		let ret = getBits(avail);
+		ret |= getBits(6-avail) << avail;
+		return ret
+	}
+
+	const numBits = array.length * 8;
+	const numCodes = Math.floor(numBits / 6);
+
+	for (let i = 0; i < numCodes; i++) {
+		const code = getCode();
+		if (code == 0) break;
+		const c = decodeLetter(code);
+		ret += c;
+	}
+
+	return ret;
+}
